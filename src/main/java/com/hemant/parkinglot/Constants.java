9157cc45fff802ddcb4ed076aad6b73a6400f5b1
hemantvsn <hemantvsn@gmail.com>
2016-12-10 14:16:30 +0530

    Additional constant messages for the cases.

--- src/main/java/com/hemant/parkinglot/Constants.java
+++ src/main/java/com/hemant/parkinglot/Constants.java
@@ -1,6 +1,14 @@
 package com.hemant.parkinglot;
 
 public class Constants {
-	public static final String PARKING_LOT_NEGATIVE_SIZE_EXC = "Cannot initialize parkingLot with size <= 0";
-	public static final String LOT_CREATED_WITH_SIZE = "Created a parking lot with %d slots ";
+	public static final String PARKING_LOT_NEGATIVE_SIZE_EXC = "Cannot initialize parkingLot with size <= 0.";
+	public static final String LOT_CREATED_WITH_SIZE = "Created a parking lot with %d slots.";
+	public static final String PARKING_IS_FULL = "Sorry, parking lot is full.";
+	public static final String CAR_ALREADY_PARKED = "Car with RegNo: %s is already parked.";
+	public static final String ALLOCATED_SLOT_NO = "Allocated slot number: %d";
+	public static final String INVALID_SLOT_NO = "Invalid slot number";
+	public static final String SLOT_ALREADY_EMPTY = "Slot is already empty";
+	public static final String SLOT_FREED = "Slot number %d is free.";
+	public static final String PARKING_LOT_NOT_CREATED_YET = "Parking lot is not created yet.";
+	public static final String STATUS_FORMAT = "%s \t\t %s \t\t %s \t\t \n";
 }
