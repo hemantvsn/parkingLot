0ffdeb67c6c3f6ddb000617327acbfdef2118255
hemantvsn <hemantvsn@gmail.com>
2016-12-10 17:26:30 +0530

    Changed the bash file, so it can be executed from anywhere.

--- my_program.sh
+++ my_program.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-java -jar target/parkinglot-0.0.1-SNAPSHOT.jar $1
+
+current_dir=$(pwd)
+script_dir=$(dirname $0)
+
+echo $current_dir
+echo $script_dir
+java -jar $script_dir/target/parkinglot-0.0.1-SNAPSHOT.jar $1
